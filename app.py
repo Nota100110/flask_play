31039b8876b84be0e45f3683601ceeddf88360aa
pineappleBottom <ibtisam.sheikh7@outlook.com>
2022-03-09 20:23:42 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+#1) application that serves your website - file that creates flask app
+
+#module currently running in flask
+app = Flask(__name__)
+
+#print(__name__)
+
+# first route
+@app.route('/')
+
+def home():
+    #returns page from template just created
+    return render_template('home.html')
+
+# Second route
+@app.route('/about')
+
+def about():
+    # returns text url shortener
+    return 'This is a URL shortener'
+
+
